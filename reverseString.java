1b83e7d88ef5d2121d8601a37c9fbd36d8ea2db3
Alex Sanchez <alex@Alexs-MacBook-Air.local>
2020-01-14 16:39:25 -0700

    Created the implentation in java

--- reverseString.java
+++ reverseString.java
@@ -1,18 +1,19 @@
 
 public class reverseString {
 
-    static void main(String[] args){
+    public static void main(String[] args){
     
         String name = "Sanchez";
         char[] reversedString = new char[name.length()];
         for(int i = 0;i<name.length();i++){
-            char a = name.charAt(1);
+            char a = name.charAt(name.length() - i - 1);
             reversedString[i] = a;
         }
 
         for(int i = 0;i<name.length();i++){
             System.out.print(reversedString[i]);
         }
+        System.out.println("");
     }
 }
 
