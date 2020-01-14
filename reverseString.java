8a4261d083cd3040ec9bc6a732131ec481d0c94c
Alex Sanchez <alex@Alexs-MacBook-Air.local>
2020-01-14 16:39:57 -0700

    Added Comments

--- reverseString.java
+++ reverseString.java
@@ -2,7 +2,7 @@
 public class reverseString {
 
     public static void main(String[] args){
-    
+    // Reversed string by putting reverse string in an array
         String name = "Sanchez";
         char[] reversedString = new char[name.length()];
         for(int i = 0;i<name.length();i++){
